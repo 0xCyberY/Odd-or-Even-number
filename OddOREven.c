461acdeebc9c1e5d05ea762d05a3cd349c39047c
ABDULLAH BAGHUTH <36686812+abdullah-baghuth@users.noreply.github.com>
2019-10-14 00:31:28 +0530

    source code

---
+++ OddOREven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number to check withere odd or even: \n");
+	scanf("%d",&n);
+	if(n%2==0)
+		printf("the number is Even\n");
+	else
+		printf("the number is odd\n");
+return 0;
+}
+
